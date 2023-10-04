4c3944e2f272816259fd27929f28999189bbbd1d
cskiwi <glenn.latomme@gmail.com>
2023-10-04 09:56:47 +0200

    feat: host client from server

--- apps/server/src/app/app.module.ts
+++ apps/server/src/app/app.module.ts
@@ -2,9 +2,21 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { environment } from '../environments/environment';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+
+const productionModules = environment.production
+  ? [
+      ServeStaticModule.forRoot({
+        rootPath: join(__dirname, '..', 'client'),
+        exclude: ['/api*'],
+      }),
+    ]
+  : [];
 
 @Module({
-  imports: [],
+  imports: [...productionModules],
   controllers: [AppController],
   providers: [AppService],
 })
